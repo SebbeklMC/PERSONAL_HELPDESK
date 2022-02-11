f2e975ee5ef7ed94fb71fb715203c0b17971f860
SebbeklMC <luis36665789@gmail.com>
2022-02-10 19:32:29 -0600

    Integracion del Proyecto a GIT

---
+++ view/Home/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php require_once("../MainHead/head.php"); ?>
+<title>Beliveo</>::Home</title>
+</head>
+
+<body class="with-side-menu">
+
+    <?php require_once("../MainHeader/header.php"); ?>
+
+    <div class="mobile-menu-left-overlay"></div>
+
+    <?php require_once("../MainNav/nav.php"); ?>
+
+    <div class="page-content">
+        <div class="container-fluid">
+            Blank page.
+        </div>
+    </div>
+    <?php require_once("../MainJS/js.php"); ?>
+</body>
+
+</html>
---
+++ view/MainHeader/header.php
@@ -0,0 +1,49 @@
+<header class="site-header">
+    <div class="container-fluid">
+
+        <a href="#" class="site-logo">
+            <img class="hidden-md-down" src="../../public/img/logo-2.png" alt="">
+            <img class="hidden-lg-up" src="../../public/img/logo-2-mob.png" alt="">
+        </a>
+
+        <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
+            <span>toggle menu</span>
+        </button>
+
+        <button class="hamburger hamburger--htla">
+            <span>toggle menu</span>
+        </button>
+
+        <div class="site-header-content">
+            <div class="site-header-content-in">
+                <div class="site-header-shown">
+                    <div class="dropdown user-menu">
+                        <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" arial-haspop="true" arial-expanded="false">
+                            <img src="../../public/img/avatar-2-64.png" alt="">
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+                            <a class="dropdown-item" href="../Perfil/"><span class="font-icon glyphicon glyphicon-user"></span>perfil</a>
+                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-question-sign"></span>ayuda</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="../Logout/"><span class="font-icon glyphicon glyphicon-log-out"></span>Cerrar Sesion</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mobile-menu-right-overlay"></div>
+
+                <div class="dropdown dropdown-typical">
+                    <a href="#" class="dropdown-toggle no-arr">
+                        <span class="font-icon font-icon-home"></span>
+                    </a>
+                </div>
+
+                <div class="dropdown dropdown-typical">
+                    <a href="#" class="dropdown-toggle no-arr">
+                        <span class="font-icon font-icon-user"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
