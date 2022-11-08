327949d4697bac87368f86271fe2a9c1004f416a
Fernando Calvario <fernando.calvario@beliveo.com>
2022-11-08 12:48:05 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,4 @@
-<?php
-    require_once("config/conexion.php");
-    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
-        require_once("models/Usuario.php");
-        $usuario = new Usuario();
-        $usuario->login();
-    }
-?>
+
 <!DOCTYPE html>
 <html>
 <head lang="es">
