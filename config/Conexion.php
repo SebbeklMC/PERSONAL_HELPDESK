2a1193d760d68c16e6e08f68a0c5587760fa5862
Fernando Calvario <luis36665789@gmail.com>
2022-11-08 23:18:09 -0600

    Cambios prod

--- config/Conexion.php
+++ config/Conexion.php
@@ -1,35 +1,37 @@
 <?php
-    /* TODO: Inicio de Sesion en la WebApp */
-    session_start();
+/* TODO: Inicio de Sesion en la WebApp */
+session_start();
 
-    class Conectar{
-        protected $dbh;
+class Conectar
+{
+    protected $dbh;
 
-        protected function Conexion(){
-            try {
-                //TODO: Cadena de Conexion Local
-				//$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=fernando_helpdesk","root","");
-                //TODO: Cadenad e Conexion Produccion
-                $conectar = $this->dbh = new PDO("mysql:host=ls-eb63f4de77556b4015d673d3269fd3adbf2783d5.c4do5q4y1jfp.us-east-2.rds.amazonaws.com;dbname=fernando_helpdesk","dbmasteruser","Fernando123");
-				return $conectar;
-			} catch (Exception $e) {
-				print "¡Error BD!: " . $e->getMessage() . "<br/>";
-				die();
-			}
+    protected function Conexion()
+    {
+        try {
+            //TODO: Cadena de Conexion Local
+            //$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=fernando_helpdesk","root","");
+            //TODO: Cadenad e Conexion Produccion
+            $conectar = $this->dbh = new PDO("mysql:host=ls-eb63f4de77556b4015d673d3269fd3adbf2783d5.c4do5q4y1jfp.us-east-2.rds.amazonaws.com;dbname=fernando_helpdesk", "dbmasteruser", "Fernando123");
+            return $conectar;
+        } catch (Exception $e) {
+            print "¡Error BD!: " . $e->getMessage() . "<br/>";
+            die();
         }
-
-        /* TODO: Set Name para utf 8 español - evitar tener problemas con las tildes */
-        public function set_names(){
-			return $this->dbh->query("SET NAMES 'utf8'");
-        }
-
-        /* TODO: Ruta o Link del proyecto */
-        public static function ruta(){
-            //TODO: Ruta Proyecto Local
-			//return "http://localhost:90/PERSONAL_HelpDesk/";
-            //TODO: Ruta Proyecto Produccion
-            return "https://d3k1037fmh5ncy.cloudfront.net/";
-		}
-
     }
-?>
+
+    /* TODO: Set Name para utf 8 español - evitar tener problemas con las tildes */
+    public function set_names()
+    {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+
+    /* TODO: Ruta o Link del proyecto */
+    public static function ruta()
+    {
+        //TODO: Ruta Proyecto Local
+        //return "http://localhost:90/PERSONAL_HelpDesk/";
+        //TODO: Ruta Proyecto Produccion
+        return "https://137.184.246.214/PERSONAL_HELPDESK";
+    }
+}
